2a0364b373ceda48353437798f4765efb31d4327
FirqySutan17 <firqy.sutan@gmail.com>
2024-08-09 15:03:58 +0700

    Merging

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -81,7 +81,7 @@ class _DashboardState extends State<Dashboard> {
 
   String urlAPIInternal = 'http://103.209.6.32:8080/cct-api/api';
   String urlAPI = 'http://103.209.6.32:8080/cct-api/api';
-  // String urlAPI = 'http://10.137.26.67:8080/cct-api/api';
+
   bool _isLoading = false;
   bool isIncludeData = false;
 
